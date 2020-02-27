85797477d5c71814f75ff36ba05602d3167f6b19
Alethia Quintero <alethiaq22@gmail.com>
2020-02-26 23:05:01 -0500

    Imported FormsModule, 1st use of directive- ngModel

--- angular-practice/src/app/app.module.ts
+++ angular-practice/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+// here we imported the FormsModule package and added it to our imports list to be used in out html file.. it gives us some tools we can use for inputs and all different kinds of forms
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
